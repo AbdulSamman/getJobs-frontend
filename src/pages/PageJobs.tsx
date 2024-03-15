eac50fc755905c06b49a4674017c82c31be06a93
AbdulSamman <sammanab@outlook.de>
2024-03-15 01:53:08 +0100

    data from axios works

--- src/pages/PageJobs.tsx
+++ src/pages/PageJobs.tsx
@@ -1,13 +1,23 @@
-import _jobs from "../data/jobs.json";
+import { useState, useEffect } from "react";
 import { IJob } from "../interfaces";
+import axios from "axios";
 
-const jobs: IJob[] = _jobs.sort((a, b) => {
-  const dateA = new Date(a.publicationDate);
-  const dateB = new Date(b.publicationDate);
-  return dateB.getTime() - dateA.getTime();
-});
+const backendUrl = "http://localhost:3668";
 
 export const PageJobs = () => {
+  const [jobs, setJobs] = useState<IJob[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = (await axios.get<IJob[]>(`${backendUrl}/jobs`)).data;
+      const _jobs: IJob[] = response.sort((a, b) => {
+        const dateA = new Date(a.publicationDate);
+        const dateB = new Date(b.publicationDate);
+        return dateB.getTime() - dateA.getTime();
+      });
+      setJobs(_jobs);
+    })();
+  });
   return (
     <div className="page pageJobs">
       <p>There are {jobs.length} jobs:</p>
